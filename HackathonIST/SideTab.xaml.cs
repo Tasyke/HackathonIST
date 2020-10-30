21bb582eb4a635f377d084a0b89a34fd6d74b94d
Tasyke <takeeuq@gmail.com>
2020-10-30 22:23:56 +0400

    Добавьте файлы проекта.

---
+++ HackathonIST/SideTab.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HackathonIST
+{
+    public partial class HackathonIST : Shell
+    {
+        public HackathonIST()
+        {
+            Items.Add(new FlyoutItem
+            {
+                Title = "Авторизация",
+                Items =
+                {
+                    new Tab
+                    {
+                        Items = { new ShellContent {Content = new Authorization() } }
+                }
+            }
+            });
+        }
+    }
+}
