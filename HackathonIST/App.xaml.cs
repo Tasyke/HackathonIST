9c69f30680dd075cc1bc47fc970e66360c7c8b10
Tasyke <takeeuq@gmail.com>
2020-10-31 11:28:34 +0400

    Улучшил интерфейс, кое-что поменял

--- HackathonIST/App.xaml.cs
+++ HackathonIST/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HackathonIST
         {
             InitializeComponent();
 
-            MainPage = new Authorization();
+            MainPage = new BuilderStart();
         }
 
         protected override void OnStart()
